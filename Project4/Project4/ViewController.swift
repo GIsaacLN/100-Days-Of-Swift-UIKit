c6fdff2d633e9a462b5e2160d5463d373625dad3
Isaac Lopez <isaac.lopez.1b@gmail.com>
2025-03-30 12:22:49 -0600

    DAY 24
    Project 4, part one
    - Setting up
    - Creating a simple browser with WKWebView
    - Choosing a website: UIAlertController action sheets

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Gustavo Isaac Lopez Nunez on 30/03/25.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.hivecents.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "konpa.app", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hivecents.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(_ action: UIAlertAction) {
+        guard let urlString = action.title else { return }
+        guard let url = URL(string: "https://" + urlString) else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+
+}
+
