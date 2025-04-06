25eaf10955bf9367774e990ba4e8db6aee14844b
Isaac Lopez <88524927+GIsaacLN@users.noreply.github.com>
2025-04-06 12:10:55 -0600

    DAY 29
    Project 5, part three
    - Wrap up
    - Review for Project 5: Word Scramble

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -7,15 +7,35 @@
 
 import UIKit
 
+enum GameError: Error {
+    case notValid
+    case notUnique
+    case notAWord
+}
+
+extension GameError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .notValid:
+            return "Word can't be built out of the letters from the current word."
+        case .notUnique:
+            return "Be more original! That word has already been used."
+        case .notAWord:
+            return "Words must be real English words."
+        }
+    }
+}
+
 class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
-
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Restart", style: .plain, target: self, action: #selector(startGame))
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL, encoding: .utf8) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -29,7 +49,7 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -60,36 +80,48 @@ class ViewController: UITableViewController {
     
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
-        
-        let errorTitle: String
-        let errorMessage: String
-        
-        if isPossible(word: lowerAnswer) {
-            if isOriginal(word: lowerAnswer) {
-                if isReal(word: lowerAnswer) {
-                    usedWords.insert(lowerAnswer, at: 0)
-                    
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-                    
-                    return
-                } else {
-                    errorTitle = "Not a real word"
-                    errorMessage = "Words must be real."
-                }
-            } else {
-                errorTitle = "Word has already been used"
-                errorMessage = "Be more original."
+        do {
+            if try checkWord(lowerAnswer) {
+                usedWords.insert(lowerAnswer, at: 0)
+                let indexPath = IndexPath(row: 0, section: 0)
+                tableView.insertRows(at: [indexPath], with: .automatic)
+                return
             }
-        } else {
-            guard let title = title?.lowercased() else { return }
-            errorTitle = "Not a valid word"
-            errorMessage = "Word can't be built out of the letters from \(title)."
+        } catch {
+            guard let error = error as? GameError else { return }
+            showErrorMessage(error)
+        }
+    }
+    
+    func showErrorMessage(_ error: GameError) {
+        let errorTitle: String
+        let errorMessage = error.localizedDescription
+        
+        switch error {
+        case .notValid:
+            errorTitle =  "Not a valid word."
+        case .notUnique:
+            errorTitle = "Word has already been used."
+        case .notAWord:
+            errorTitle = "Not a real word."
         }
         
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    func checkWord(_ word: String) throws -> Bool {
+        if !isPossible(word: word) {
+            throw GameError.notValid
+        }
+        if !isOriginal(word: word) {
+            throw GameError.notUnique
+        }
+        if !isReal(word: word) {
+            throw GameError.notAWord
+        }
+        return true
     }
     
     func isPossible(word: String) -> Bool {
@@ -107,10 +139,12 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal(word: String) -> Bool {
+        if word == title?.lowercased() { return false }
         return !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool{
+        if word.count < 3 { return false }
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
