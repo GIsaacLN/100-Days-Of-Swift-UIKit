f9361f953a91077d5b024d959f31b30839c2cc87
Isaac Lopez <88524927+GIsaacLN@users.noreply.github.com>
2025-04-11 10:25:33 -0600

    DAY 31
    Project 6, part two
    - Auto Layout metrics and priorities: constraints(withVisualFormat:)
    - Auto Layout anchors
    - Wrap up
    - Review for Project 6: Auto Layout

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -43,11 +43,31 @@ class ViewController: UIViewController {
             view.addSubview(label)
         }
 
+        /*
         for label in viewsDictionary.keys {
             view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
         
-        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        let metrics = ["labelHeight": 72]
+        
+        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+         */
+        
+        var previousLabel: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+            if let previousLabel = previousLabel {
+                label.topAnchor.constraint(equalTo: previousLabel.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            previousLabel = label
+        }
+        
     }
 
 
