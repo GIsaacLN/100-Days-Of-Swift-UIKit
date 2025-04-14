5847cdb715d0c69a68e1231312c5c7ead2c0e8bb
Isaac Lopez <88524927+GIsaacLN@users.noreply.github.com>
2025-04-14 15:51:52 -0600

    DAY 33
    Project 7, part one
    - Setting up
    - Creating the basic UI: UITabBarController
    - Parsing JSON using the Codable protocol

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Gustavo Isaac Lopez Nunez on 14/04/25.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
