6acb8938b16bb7f064697ac36c037130db819e74
Isaac Lopez <88524927+GIsaacLN@users.noreply.github.com>
2025-05-15 21:17:16 -0600

    DAY 52
    Project 13, part one
    - Setting up
    - Designing the interface
    - Importing a picture

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Gustavo Isaac Lopez Nunez on 15/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importImage))
+    }
+    
+    @objc func importImage() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+}
+
