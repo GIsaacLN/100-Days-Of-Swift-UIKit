7ff4f2a596e5fba065a3101ef00cefd60a607ff4
Isaac Lopez <88524927+GIsaacLN@users.noreply.github.com>
2025-05-28 10:19:35 -0600

    DAY 60
    Project 16, part one
    - Setting up
    - Up and running with MapKit
    - Annotations and accessory views: MKPinAnnotationView

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Gustavo Isaac Lopez Nunez on 28/05/25.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+
+}
