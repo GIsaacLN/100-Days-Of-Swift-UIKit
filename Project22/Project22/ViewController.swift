d75808ce2c0ccfba12a661f579b0508f27aaf3b5
Isaac Lopez <88524927+GIsaacLN@users.noreply.github.com>
2025-06-18 12:54:14 -0600

    DAY 76
    Project 22, part two
    - Wrap up
    - Review for Project 22: Detect-a-Beacon

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var distanceReading: UILabel!
+    @IBOutlet var beaconName: UILabel!
+    @IBOutlet var circle: UIView!
     var locationManager: CLLocationManager?
+    var hasBeaconDetected: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +21,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.requestAlwaysAuthorization()
-        
+        circle.backgroundColor = .systemBackground
+        circle.layer.cornerRadius = 128
+        circle.layer.zPosition = -1
         view.backgroundColor = .gray
     }
     
@@ -33,11 +38,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func startScanning() {
-        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        let beacons = ["5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0", "74278BDA-B644-4520-8F0C-720EAF059935"]
         
-        locationManager?.startMonitoring(for: beaconRegion)
-        locationManager?.startRangingBeacons(in: beaconRegion)
+        for (i, beacon) in beacons.enumerated() {
+            let uuid = UUID(uuidString: beacon)!
+            let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon-\(i+1)")
+                        
+            locationManager?.startMonitoring(for: beaconRegion)
+            locationManager?.startRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
+        }
     }
 
     func update(distance: CLProximity) {
@@ -46,25 +55,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             case .far:
                 self.distanceReading.text = "FAR"
                 self.view.backgroundColor = .blue
+                self.circle.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
             case .near:
                 self.distanceReading.text = "NEAR"
                 self.view.backgroundColor = .orange
+                self.circle.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
             case .immediate:
                 self.distanceReading.text = "RIGHT HERE"
                 self.view.backgroundColor = .red
+                self.circle.transform = CGAffineTransform(scaleX: 1, y: 1)
              default:
+                self.beaconName.text = "UNKNOWN BEACON"
                 self.distanceReading.text = "UNKNOWN"
                 self.view.backgroundColor = .gray
+                self.circle.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
             }
         }
     }
-
-    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if let beacon = beacons.first {
-            update(distance:  beacon.proximity)
-        } else {
-            update(distance: .unknown)
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        guard let beacon = beacons.first else { return }
+        if !hasBeaconDetected {
+            hasBeaconDetected = true
+            
+            let ac = UIAlertController(title: "Beacon Detected", message: "A beacon has been detected for the first time", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(ac, animated: true)
         }
+        beaconName.text = beacon.uuid.uuidString
+        update(distance:  beacon.proximity)
+
     }
 }
 
